7507d0c2c064126179402f207bb05f0f2620e5f6
Baoxu Shi <baoxu.shi@gmail.com>
2012-11-09 12:54:29 +0800

    change configure style

--- configuration.js
+++ configuration.js
@@ -12,7 +12,22 @@ module.exports = function(){
             return {}
         case 'development':
             return {
-                WebSocketPort   :   9876
+                WebSocketPort   :   9876,
+                mongo:{
+                    host : '127.0.0.1',
+                    port : 27017,
+                    db : 'letter_press_test',
+                    options : {
+                        auto_reconnect : true
+                    }
+                },
+                redisHost    :   '127.0.0.1',
+                redisPort    :   6379,
+                redisConnectionPoolDB : 0,
+                redisPushDB : 1,
+                mongoHost    :   '127.0.0.1',
+                mongoPort  :    27017,
+                mongoDB :   'user'
             }
         default:
     }
