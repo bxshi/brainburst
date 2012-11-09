e75c4026c3619c311bb8c3444433a84f4917b358
Baoxu Shi <baoxu.shi@gmail.com>
2012-11-09 15:38:58 +0800

    add logger

--- libs/RedisConnection.js
+++ libs/RedisConnection.js
@@ -1,10 +1,17 @@
 var redis = require('redis');
 
+var logger = require('./logger.js')
+
 var RedisConnection = function(config) {
 	this.client = redis.createClient(config.port, config.host, config.options);
 	this.client.on('error', function(err){
-		console.log('redis error : ' + err);
+		logger.error('redis PushDB error : ' + err);
 	});
+    this.client.select(config.redisPushDB, function(err){
+        if(err){
+            logger.error('select redis PushDB '+config.redisPushDB+' error : ' + err);
+        }
+    });
 }
 RedisConnection.prototype.flushdb = function() {
 	this.client.flushdb();
