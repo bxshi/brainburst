92e7339c858d13c489f40fda88817f0116073b36
Baoxu Shi <baoxu.shi@gmail.com>
2012-11-09 10:59:18 +0800

    delete a func

--- libs/MongoDBConnection.js
+++ libs/MongoDBConnection.js
@@ -1,58 +1,58 @@
 var mongo = require('mongodb');
 
 var MongoDBConnection = function(config) {
-	this.host = config.host;
-	this.port = config.port;
-	this.db = config.db;
-	this.options = config.options;
-	this.queue = [];
-	this.db_handler = null;
-}
+    this.host = config.host;
+    this.port = config.port;
+    this.db = config.db;
+    this.options = config.options;
+    this.queue = [];
+    this.db_handler = null;
+//}
 
-MongoDBConnection.prototype.open = function() {
-	this.server = new mongo.Server(this.host, this.port, this.options);
-	this.db_object = new mongo.Db(this.db, this.server, {safe:false});
-	var self = this;
-	this.db_object.open(function(err, db_handler) {
-		self.db_handler = db_handler;
-		var queue = self.queue;
-		for(var i = 0; i < queue.length; i++) {
-			if(queue[i].collectionName != null) {
-				collection = new mongo.Collection(queue[i].collectionName, db_handler);
-				queue[i].callback(collection);
-			}else {
-				queue[i].callback(db_handler);
-			}
-		}
-		self.queue = [];
-	});
+//MongoDBConnection.prototype.open = function() {
+    this.server = new mongo.Server(this.host, this.port, this.options);
+    this.db_object = new mongo.Db(this.db, this.server, {safe:false});
+    var self = this;
+    this.db_object.open(function(err, db_handler) {
+        self.db_handler = db_handler;
+        var queue = self.queue;
+        for(var i = 0; i < queue.length; i++) {
+            if(queue[i].collectionName != null) {
+                collection = new mongo.Collection(queue[i].collectionName, db_handler);
+                queue[i].callback(collection);
+            }else {
+                queue[i].callback(db_handler);
+            }
+        }
+        self.queue = [];
+    });
 }
 MongoDBConnection.prototype.close = function() {
-	this.db_object.close();
+    this.db_object.close();
 }
 
 MongoDBConnection.prototype.query = function(collectionName, callback) {
-	if(this.db_handler) {
-		var collection = new mongo.Collection( this.db_handler, collectionName);
-		callback(collection);
-		return 
-	}
-	this.queue.push({'collectionName': collectionName, 
-			'callback' : callback});
+    if(this.db_handler) {
+        var collection = new mongo.Collection( this.db_handler, collectionName);
+        callback(collection);
+        return
+    }
+    this.queue.push({'collectionName': collectionName,
+        'callback' : callback});
 }
 MongoDBConnection.prototype.dbOperation = function( callback) {
-	if(this.db_handler) {
-		callback(this.db_handler);
-		return ;
-	}
-	this.queue.push({'callback' : callback});
+    if(this.db_handler) {
+        callback(this.db_handler);
+        return ;
+    }
+    this.queue.push({'callback' : callback});
 }
 MongoDBConnection.prototype.drop = function(callback) {
-	this.dbOperation(function(db_handler) {
-		db_handler.dropDatabase(function(err) {
-			callback();
-		});
-	});
+    this.dbOperation(function(db_handler) {
+        db_handler.dropDatabase(function(err) {
+            callback();
+        });
+    });
 }
 
 
--- libs/PlayerDAO.js
+++ libs/PlayerDAO.js
@@ -3,26 +3,27 @@ var mongo = require('mongodb');
 var collectionName = "players";
 
 var PlayerDAO = function(connection) {
-	this.connection = connection;
+    this.connection = connection;
 }
 
 PlayerDAO.prototype.getPlayerById = function(id, cb) {
-	this.connection.query(collectionName, function(collection){
-		collection.findOne({user_id : id}, function(err, doc){
-			var player = doc;
-			cb(player);
-		});
-	});
+    this.connection.query(collectionName, function(collection){
+        collection.findOne({user_id : id}, function(err, doc){
+            var player = doc;
+            cb(player);
+        });
+    });
 }
 
 PlayerDAO.prototype.createPlayer = function(player, cb) {
-	this.connection.query(collectionName, function(collection) {
-		var doc = player
-		collection.insert(doc, function(err){
-			cb();
-		});
-	});
+    if(player.user_id == null)
+        throw "player has no user_id";
+    this.connection.query(collectionName, function(collection) {
+        var doc = player
+        collection.insert(doc, function(err){
+            cb();
+        });
+    });
 }
 
 exports.PlayerDAO = PlayerDAO;
-
