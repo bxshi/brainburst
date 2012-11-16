23c8703aa8cdd7e211675e7b9416bba1635c7e8a
Baoxu Shi <baoxu.shi@gmail.com>
2012-11-16 17:04:31 +0800

    add test config

--- libs4test/configuration.js
+++ libs4test/configuration.js
@@ -7,13 +7,19 @@
  */
 
 exports.websocket = {
-  host : "ws://127.0.0.1",
+  host : "ws://192.168.2.202",
   port : "9876",
   protocol : "brain_burst"
 };
 
-exports.worker_number = 10;
+exports.websocketRemote = {
+    host : "ws://192.168.2.202",
+    port : "9876",
+    protocol : "brain_burst"
+};
 
+exports.worker_number = 10;
+exports.concurrency_number = 1000;
 exports.mongo = {
     host : '127.0.0.1',
     port : 27017,
