f5e634a7bc4fee20a9552411b53a12bed17ee44f
Baoxu Shi <baoxu.shi@gmail.com>
2012-12-07 11:31:38 +0800

    new stress test for linode

--- test/StressTest.js
+++ test/StressTest.js
@@ -12,6 +12,10 @@ var jsonBuilder = require("../libs4test/clientJSONBuilder.js");
 var concurrency_number = require("../libs4test/configuration.js").concurrency_number;
 var async = require("async");
 
+var sendData = require("../libs/common.js").sendData;
+var messageHandler = require("../libs/ClientMessageHandler.js");
+var getData = require("../libs/common.js").getData;
+
 describe('# Stress Test',function(){
 
 
@@ -24,14 +28,14 @@ describe('# Stress Test',function(){
             var tmpLatency = Date.now();
             var worker = wsCreator(wsconf.host,wsconf.port);
             worker.on('connectFailed', function(){
-                tmpLatency = Date.now() - tmpLatency;
+//                tmpLatency = Date.now() - tmpLatency;
                 disconn_count++;
-                latency +=tmpLatency;
-                if(disconn_count == concurrency_number){
-                    latency = (latency / 2) / disconn_count;
-                    console.log("!!!!!"+latency);
-                    done();
-                }
+//                latency +=tmpLatency;
+//                if(disconn_count == concurrency_number){
+//                    latency = (latency / 2) / disconn_count;
+//                    console.log("!!!!!"+latency);
+//                    done();
+//                }
             });
         };
         var parallel_task = [];
@@ -52,17 +56,64 @@ describe('# Stress Test',function(){
             var worker = wsCreator(wsconf.host,wsconf.port,wsconf.protocol);
             worker.on('connect', function(connection){
                 connection.close();
-                tmpLatency = Date.now() - tmpLatency;
-                latency +=tmpLatency;
+//                tmpLatency = Date.now() - tmpLatency;
+//                latency +=tmpLatency;
                 disconn_count++;
 //                console.log(disconn_count);
                 if(disconn_count==concurrency_number){
-                    latency = latency/2/disconn_count;
-                    console.log("!!!!!"+latency);
+//                    latency = latency/2/disconn_count;
+//                    console.log("!!!!!"+latency);
                     done();
                 }
             });
         };
+
+        var parallel_task = [];
+        for(var i=0;i<concurrency_number;i++){
+            parallel_task[i] = parallel_function;
+        }
+
+        async.parallel(parallel_task,null);
+
+    });
+
+    it('## connection, then create two matches, submit each match for 1 seconds.', function(done){
+        var count = 0;
+        var parallel_function = function(){
+            var msgHandler = new messageHandler();
+            var worker = wsCreator(wsconf.host, wsconf.port, wsconf.protocol);
+            worker.on('connect', function(connection){
+                var JSON2Send = JSON.stringify(jsonBuilder.user_login_builder(null, {'nickname':'stress_test'}));
+                sendData(connection, JSON2Send);
+                connection.on('message', function(message){
+                    getData(connection, message.binaryData, msgHandler);
+                });
+            });
+            msgHandler.on('UserLogin', function(connection, message){
+                var JSON2Send = JSON.stringify(jsonBuilder.create_match_builder('auto',message.user,{'match_data':"stressTest"}));
+                sendData(connection, JSON2Send);
+            });
+            msgHandler.on('BotMatch', function(connection, message){
+                var JSON2Send = JSON.stringify(jsonBuilder.submit_match_builder(message.user, message.match));
+                setTimeout(sendData, Math.round(Math.random()*10000) % 2000, connection, JSON2Send);
+                count++;
+                if(count >= concurrency_number * 100){
+                    done();
+                }
+            });
+            msgHandler.on('UpdateMatch', function(connection, message){
+                var JSON2Send = JSON.stringify(jsonBuilder.submit_match_builder(message.user, message.match));
+                setTimeout(sendData, Math.round(Math.random()*10000) % 2000, connection, JSON2Send);
+                count++;
+                if(count >= concurrency_number * 100){
+                    done();
+                }
+            });
+            msgHandler.on('PushResponse', function(connection, message){
+                var JSON2Send = JSON.stringify(jsonBuilder.push_response_builder(message.push_id));
+                sendData(connection, JSON2Send);
+            });
+        }
 
         var parallel_task = [];
         for(var i=0;i<concurrency_number;i++){
