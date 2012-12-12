1989310b2ac855143260ddb8238be9f42cf32ad8
Baoxu Shi <baoxu.shi@gmail.com>
2012-12-12 19:15:37 +0800

    fix nick_name to nickname

--- Bots/LetterPressBot/InitBot.js
+++ Bots/LetterPressBot/InitBot.js
@@ -11,21 +11,17 @@ const conf = new Conf();
 
 var mongo = require('../../libs/MongoDBConnection.js');
 var mongoClient = new mongo.MongoDBConnection(conf.mongo);
-var messageHandler = require('../../libs/MessageHandler.js');
-const msgHandler = new messageHandler();
-const sendData = require('../../libs/common.js').sendData;
 var player = require('../../libs/PlayerDAO.js');
 var match = require('../../libs/MatchDAO.js');
 
 // DAOs
-var matchDAO = new match.MatchDAO(mongoClient);
 var playerDAO = new player.PlayerDAO(mongoClient);
 
 var Bots = require("./BotConfig.js").BotConfigs;
 
 
 for(var i = 0; i< Bots.length; i++){
-    var botInfo = {'user_id' : Bots[i].uuid, 'user_data':{'nick_name':Bots[i].nickname}};
+    var botInfo = {'user_id' : Bots[i].uuid, 'user_data':{'nickname':Bots[i].nickname}};
     playerDAO.createPlayer(botInfo, function(){
         console.log("bot created");
     });
